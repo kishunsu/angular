43ec1ca5522bf4b5d105b5f2a22d8830e5c7d444
trainee-shunsuke.b.kimura <trainee-shunsuke.b.kimura@accenture.com>
2020-09-27 17:35:38 +0900

    SPA対応

---
+++ reservation/src/app/products.ts
@@ -0,0 +1,82 @@
+export const products = [
+    {
+        coverImage: './../../../assets/img/phone-cover.jpg',
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens',
+        heading: [
+            {
+                name: 'heading1',
+                description: 'heading1 description',
+            },
+            {
+                name: 'heading2',
+                description: 'heading2 description',
+            },
+            {
+                name: 'heading3',
+                description: 'heading3 description',
+            },
+        ]
+    },
+    {
+        coverImage: './../../../assets/img/phone-cover.jpg',
+        name: 'Phone Mini',
+        price: 699,
+        description: 'A great phone with one of the best cameras',
+        heading: [
+            {
+                name: 'heading1',
+                description: 'heading1 description',
+            },
+            {
+                name: 'heading2',
+                description: 'heading2 description',
+            },
+            {
+                name: 'heading3',
+                description: 'heading3 description',
+            },
+        ]
+    },
+    {
+        coverImage: './../../../assets/img/phone-cover.jpg',
+        name: 'Phone Standard',
+        price: 299,
+        description: 'A standard phone',
+        heading: [
+            {
+                name: 'heading1',
+                description: 'heading1 description',
+            },
+            {
+                name: 'heading2',
+                description: 'heading2 description',
+            },
+            {
+                name: 'heading3',
+                description: 'heading3 description',
+            },
+        ]
+    },
+    {
+        coverImage: './../../../assets/img/phone-cover.jpg',
+        name: 'Phone Special',
+        price: 999,
+        description: 'A special phone',
+        heading: [
+            {
+                name: 'heading1',
+                description: 'heading1 description',
+            },
+            {
+                name: 'heading2',
+                description: 'heading2 description',
+            },
+            {
+                name: 'heading3',
+                description: 'heading3 description',
+            },
+        ]
+    }
+]
