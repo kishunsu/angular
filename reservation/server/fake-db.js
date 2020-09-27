8a891f9f9e293123f21d5520a1c5f15dc140f304
trainee-shunsuke.b.kimura <trainee-shunsuke.b.kimura@accenture.com>
2020-09-28 00:09:38 +0900

    バックエンド処理追加

---
+++ reservation/server/fake-db.js
@@ -0,0 +1,109 @@
+const Product = require('./model/product');
+
+class FakeDb {
+    constructor() {
+        this.products = [
+            {
+                coverImage: './../../../assets/img/phone-cover.jpg',
+                name: 'Phone XL',
+                price: 799,
+                description: 'A large phone with one of the best screens',
+                // head1: 'name'
+                heading: [
+                    {
+                        name: 'heading1',
+                        description: 'heading1 description',
+                    },
+                    {
+                        name: 'heading2',
+                        description: 'heading2 description',
+                    },
+                    {
+                        name: 'heading3',
+                        description: 'heading3 description',
+                    },
+                ]
+            },
+            {
+                coverImage: './../../../assets/img/phone-cover.jpg',
+                name: 'Phone Mini',
+                price: 699,
+                description: 'A great phone with one of the best cameras',
+                // head1: 'name'
+                heading: [
+                    {
+                        name: 'heading1',
+                        description: 'heading1 description',
+                    },
+                    {
+                        name: 'heading2',
+                        description: 'heading2 description',
+                    },
+                    {
+                        name: 'heading3',
+                        description: 'heading3 description',
+                    },
+                ]
+            },
+            {
+                coverImage: './../../../assets/img/phone-cover.jpg',
+                name: 'Phone Standard',
+                price: 299,
+                description: 'A standard phone',
+                // head1: 'name'
+                heading: [
+                    {
+                        name: 'heading1',
+                        description: 'heading1 description',
+                    },
+                    {
+                        name: 'heading2',
+                        description: 'heading2 description',
+                    },
+                    {
+                        name: 'heading3',
+                        description: 'heading3 description',
+                    },
+                ]
+            },
+            {
+                coverImage: './../../../assets/img/phone-cover.jpg',
+                name: 'Phone Special',
+                price: 999,
+                description: 'A special phone',
+                // head1: 'name'
+                heading: [
+                    {
+                        name: 'heading1',
+                        description: 'heading1 description',
+                    },
+                    {
+                        name: 'heading2',
+                        description: 'heading2 description',
+                    },
+                    {
+                        name: 'heading3',
+                        description: 'heading3 description',
+                    },
+                ]
+            }
+        ]
+    }
+    pushProductsDb(){
+        this.products.forEach((product)=>{
+            const newProduct = new Product(product)
+            newProduct.save(function(err){
+                if(!err){
+                    console.log('saved!');
+                }
+
+            });
+        }
+        )
+    }
+    seeDb(){
+        this.pushProductsDb();
+    }
+}
+
+module.exports = FakeDb;
